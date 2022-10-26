3932b084ea821be3d60eca209b7e9d398b037099
yakravets <yar.kravets@ukr.net>
2022-10-26 18:00:06 +0300

    add table buttons

--- Data/Product.cs
+++ Data/Product.cs
@@ -1,4 +1,20 @@
 ﻿namespace BlazorServerSignalRApp.Data
 {
-    public record Product(int Id, string Name, string ImageUrl, double Price);
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public double Price { get; set; }
+        public int Count { get; set; } = 0;
+
+        public Product(int id, string name, string imageUrl, double price, int count = 0)
+        {
+            Id = id;
+            Name = name;
+            ImageUrl = imageUrl;
+            Price = price;
+            Count = count;
+        }
+    }
 }
---
+++ wwwroot/js/app.js
@@ -0,0 +1,84 @@
+﻿let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+
+let item = "";
+
+function onClickAdd(product_id) {
+
+    let btn_add_name = `btnadd${product_id}`;
+    let badge_name = `badge${product_id}`;
+    let btn_rm_name = `btnrm${product_id}`;
+
+    let badge = document.getElementById(badge_name);
+
+    const count = parseInt(badge.innerText);
+    if (isNaN(count) || count == 0) {
+        badge.innerText = 1;
+        badge.classList.remove("hidden");
+        badge.classList.add("visibility");
+
+        let btn = document.getElementById(btn_add_name);
+        btn.innerText = '+';
+
+        let btn_rm = document.getElementById(btn_rm_name);
+        btn_rm.classList.remove("display-none");
+        btn_rm.classList.add("display-inline-block");
+
+    }
+    else {
+        badge.innerText = count + 1;
+    }
+
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText(`Вы выбрали товар ${product_id}!`);
+        item = toString(this.product_id);
+        tg.MainButton.show();
+    }
+}
+
+function onClickRm(product_id) {
+    let btn_add_name = `btnadd${product_id}`;
+    let badge_name = `badge${product_id}`;
+    let btn_rm_name = `btnrm${product_id}`;
+
+    let badge = document.getElementById(badge_name);
+
+    const count = parseInt(badge.innerText);
+    badge.innerText = count - 1;
+    if (count == 1) {
+        badge.classList.remove("visibility");
+        badge.classList.add("hidden");
+
+        let btn = document.getElementById(btn_add_name);
+        btn.innerText = 'ADD';
+
+        let btn_rm = document.getElementById(btn_rm_name);
+        btn_rm.classList.remove("display-inline-block");
+        btn_rm.classList.add("display-none");
+
+        if (tg.MainButton.isVisible) {
+            tg.MainButton.hide();
+        }
+    }
+}
+
+
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    tg.sendData(item);
+    tg.BackButton.show();
+});
+
+let usercard = document.getElementById("usercard");
+
+let p = document.createElement("p");
+
+p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
+
+usercard.appendChild(p); 
